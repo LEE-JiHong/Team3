6bd23309917a843616e98bcb5a9fd4452aa8bddc
KimWooGeun <dnrmsaos@gmail.com>
2020-01-03 17:35:09 +0900

    kwg

---
+++ Team3/DevForm/MaterialMgt/ISIMgt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Team3.DevForm.MaterialMgt
+{
+    //Inspection of Standard's Information 검사 기준 정보
+
+    public partial class ISIMgt : Team3.VerticalGridBaseForm
+    {
+        public ISIMgt()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
---
+++ Team3/ProcessMgt/SOO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Team3.ProcessMgt
+{
+    //State of Order's Operation
+    public partial class SOO : Team3.VerticalGridBaseForm
+    {
+        public SOO()
+        {
+            InitializeComponent();
+        }
+    }
+}
