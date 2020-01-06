6dee5cf10ceaae038881ce986e9e06368b47b561
swg8491 <tladnrbs@naver.com>
2020-01-06 18:14:27 +0900

    swg

---
+++ Team3/WorkOrder/Process_Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Team3
+{
+    public partial class Process_Inventory : Team3.VerticalGridBaseForm
+    {
+        public Process_Inventory()
+        {
+            InitializeComponent();
+        }
+    }
+}
