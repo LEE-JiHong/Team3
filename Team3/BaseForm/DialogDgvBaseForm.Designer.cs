6f57e884be03ee617fadf9511c5899eb5e47ef66
LEE-JiHong <leewlghd2@naver.com>
2020-01-06 15:57:02 +0900

    미니멈사이즈, flow레이아웃 추가

---
+++ Team3/BaseForm/DialogDgvBaseForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Team3.BaseForm
+{
+    partial class DialogDgvBaseForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(287, 532);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.Text = "닫기";
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.Size = new System.Drawing.Size(79, 19);
+            // 
+            // panel1
+            // 
+            this.panel1.Size = new System.Drawing.Size(732, 36);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel6);
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Size = new System.Drawing.Size(731, 443);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Location = new System.Drawing.Point(691, 7);
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.Location = new System.Drawing.Point(12, 22);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(708, 87);
+            this.panel4.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel5.Controls.Add(this.label2);
+            this.panel5.Location = new System.Drawing.Point(12, 140);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(211, 282);
+            this.panel5.TabIndex = 1;
+            // 
+            // panel6
+            // 
+            this.panel6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel6.Location = new System.Drawing.Point(247, 140);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(473, 282);
+            this.panel6.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(6, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 19);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "label2";
+            // 
+            // DialogDgvBaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.ClientSize = new System.Drawing.Size(778, 584);
+            this.Name = "DialogDgvBaseForm";
+            this.panel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.Panel panel6;
+        protected System.Windows.Forms.Panel panel5;
+        protected System.Windows.Forms.Panel panel4;
+        protected System.Windows.Forms.Label label2;
+    }
+}
--- Team3/BaseForm/VerticalGridBaseForm.Designer.cs
+++ Team3/BaseForm/VerticalGridBaseForm.Designer.cs
@@ -32,6 +32,7 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.TopMenu.SuspendLayout();
             this.basepanel.SuspendLayout();
             this.SuspendLayout();
@@ -45,7 +46,7 @@
             // 
             // TopMenu
             // 
-            this.TopMenu.Size = new System.Drawing.Size(914, 60);
+            this.TopMenu.Size = new System.Drawing.Size(894, 60);
             // 
             // layoutButton
             // 
@@ -57,10 +58,11 @@
             // 
             // basepanel
             // 
+            this.basepanel.Controls.Add(this.flowLayoutPanel1);
             this.basepanel.Controls.Add(this.label1);
             this.basepanel.Controls.Add(this.panel2);
             this.basepanel.Controls.Add(this.panel1);
-            this.basepanel.Size = new System.Drawing.Size(914, 642);
+            this.basepanel.Size = new System.Drawing.Size(894, 454);
             // 
             // panel2
             // 
@@ -68,9 +70,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel2.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.panel2.Location = new System.Drawing.Point(12, 158);
+            this.panel2.Location = new System.Drawing.Point(12, 173);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(890, 475);
+            this.panel2.Size = new System.Drawing.Size(870, 272);
             this.panel2.TabIndex = 9;
             // 
             // panel1
@@ -80,23 +82,34 @@
             this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.panel1.Location = new System.Drawing.Point(13, 14);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(890, 116);
+            this.panel1.Size = new System.Drawing.Size(870, 116);
             this.panel1.TabIndex = 8;
             // 
             // label1
             // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(15, 138);
+            this.label1.Location = new System.Drawing.Point(15, 149);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.Size = new System.Drawing.Size(54, 19);
             this.label1.TabIndex = 15;
             this.label1.Text = "label1";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(257, 136);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(625, 29);
+            this.flowLayoutPanel1.TabIndex = 16;
             // 
             // VerticalGridBaseForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(914, 724);
+            this.ClientSize = new System.Drawing.Size(894, 536);
+            this.MinimumSize = new System.Drawing.Size(910, 575);
             this.Name = "VerticalGridBaseForm";
             this.Text = "VerticalGridBaseForm";
             this.Load += new System.EventHandler(this.VerticalGridBaseForm_Load);
@@ -112,5 +125,6 @@
         protected System.Windows.Forms.Panel panel2;
         protected System.Windows.Forms.Panel panel1;
         protected System.Windows.Forms.Label label1;
+        protected System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
