529ae72fd668beecadd45439d576953b19dd2e33
LEE-JiHong <leewlghd2@naver.com>
2020-01-06 18:12:10 +0900

    이지홍 커밋

---
+++ Team3/BaseForm/Dialog2DgvBaseForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Team3.BaseForm
+{
+    partial class Dialog2DgvBaseForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.lblGrid1Name = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.lblGrid2Name = new System.Windows.Forms.Label();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(431, 516);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            // 
+            // label1
+            // 
+            this.label1.Size = new System.Drawing.Size(106, 19);
+            // 
+            // panel1
+            // 
+            this.panel1.Size = new System.Drawing.Size(1020, 36);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Size = new System.Drawing.Size(1019, 427);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Location = new System.Drawing.Point(979, 7);
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.panel6);
+            this.panel4.Controls.Add(this.panel7);
+            this.panel4.Controls.Add(this.lblGrid1Name);
+            this.panel4.Location = new System.Drawing.Point(21, 20);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(974, 140);
+            this.panel4.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Controls.Add(this.panel8);
+            this.panel5.Controls.Add(this.panel9);
+            this.panel5.Controls.Add(this.lblGrid2Name);
+            this.panel5.Location = new System.Drawing.Point(21, 175);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(974, 230);
+            this.panel5.TabIndex = 1;
+            // 
+            // panel7
+            // 
+            this.panel7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel7.Location = new System.Drawing.Point(653, 8);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(316, 28);
+            this.panel7.TabIndex = 19;
+            // 
+            // lblGrid1Name
+            // 
+            this.lblGrid1Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrid1Name.ForeColor = System.Drawing.Color.Black;
+            this.lblGrid1Name.Location = new System.Drawing.Point(3, 8);
+            this.lblGrid1Name.Name = "lblGrid1Name";
+            this.lblGrid1Name.Size = new System.Drawing.Size(61, 19);
+            this.lblGrid1Name.TabIndex = 18;
+            this.lblGrid1Name.Text = "label2";
+            // 
+            // panel6
+            // 
+            this.panel6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel6.Location = new System.Drawing.Point(6, 45);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(962, 81);
+            this.panel6.TabIndex = 20;
+            // 
+            // panel8
+            // 
+            this.panel8.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel8.Location = new System.Drawing.Point(6, 48);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(962, 168);
+            this.panel8.TabIndex = 23;
+            // 
+            // panel9
+            // 
+            this.panel9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel9.Location = new System.Drawing.Point(653, 10);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(316, 28);
+            this.panel9.TabIndex = 22;
+            // 
+            // lblGrid2Name
+            // 
+            this.lblGrid2Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrid2Name.ForeColor = System.Drawing.Color.Black;
+            this.lblGrid2Name.Location = new System.Drawing.Point(3, 10);
+            this.lblGrid2Name.Name = "lblGrid2Name";
+            this.lblGrid2Name.Size = new System.Drawing.Size(61, 19);
+            this.lblGrid2Name.TabIndex = 21;
+            this.lblGrid2Name.Text = "label2";
+            // 
+            // Dialog2DgvBaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.ClientSize = new System.Drawing.Size(1066, 568);
+            this.Name = "Dialog2DgvBaseForm";
+            this.panel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        protected System.Windows.Forms.Panel panel7;
+        protected System.Windows.Forms.Label lblGrid1Name;
+        protected System.Windows.Forms.Panel panel9;
+        protected System.Windows.Forms.Label lblGrid2Name;
+        protected System.Windows.Forms.Panel panel4;
+        protected System.Windows.Forms.Panel panel5;
+        protected System.Windows.Forms.Panel panel6;
+        protected System.Windows.Forms.Panel panel8;
+    }
+}
---
+++ Team3/BaseForm/Dialog2DgvBaseForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Team3.BaseForm
+{
+    public partial class Dialog2DgvBaseForm : Team3.DialogForm
+    {
+        public Dialog2DgvBaseForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
