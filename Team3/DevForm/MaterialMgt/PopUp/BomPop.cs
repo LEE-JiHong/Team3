7f5d1f31e170b8dff025306c8b7d93ee0ddac47b
KimWooGeun <dnrmsaos@gmail.com>
2020-01-06 15:18:54 +0900

    kwg

---
+++ Team3/DevForm/MaterialMgt/PopUp/BomPop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Team3.DevForm.MaterialMgt.PopUp
+{
+    public partial class BomPop : Team3.DialogForm
+    {
+        public BomPop()
+        {
+            InitializeComponent();
+        }
+    }
+}
