cdc575bbd7f3f8279e1b7ad1f9c08a77bc3acb3c
swg8491 <tladnrbs@naver.com>
2020-01-09 17:27:31 +0900

    swg

--- Team3/DevForm/ResourceMgt/PopUp/CompanyPop.Designer.cs
+++ Team3/DevForm/ResourceMgt/PopUp/CompanyPop.Designer.cs
@@ -141,6 +141,7 @@
             this.txtCodeCompany.Name = "txtCodeCompany";
             this.txtCodeCompany.Size = new System.Drawing.Size(130, 21);
             this.txtCodeCompany.TabIndex = 0;
+            this.txtCodeCompany.Text = "수정시 변경불가";
             // 
             // label3
             // 
@@ -387,11 +388,11 @@
             this.cboTypeCompany.Size = new System.Drawing.Size(130, 23);
             this.cboTypeCompany.TabIndex = 2;
             // 
-            // businessPop
+            // CompanyPop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.ClientSize = new System.Drawing.Size(808, 451);
-            this.Name = "businessPop";
+            this.Name = "CompanyPop";
             this.panel3.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
---
+++ Team3DAC/ConnectionAccess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+namespace HanSopDACLibrary
+{
+    public abstract class ConnectionAccess
+    {
+        protected string ConnectionString
+        {
+            get
+            {
+                string connStr = string.Empty;
+
+                XmlDocument configXml = new XmlDocument();
+                configXml.Load(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/DAC.xml");
+                XmlNodeList addNodes = configXml.SelectNodes("configuration/settings/add");
+                foreach(XmlNode xmlNode in addNodes)
+                {
+                    if (xmlNode.Attributes["key"].InnerText == "team3")
+                    {
+                        connStr = ((XmlCDataSection)xmlNode.ChildNodes[0]).InnerText;
+                        break;
+                    }
+                }
+
+                return connStr;
+            }
+        }
+    }
+}
