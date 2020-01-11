52fc9e7812d15799a246dfd45e900a43174cc77e
821051984868 <dnrmsaos@gmail.com>
2020-01-11 21:13:19 +0900

    kwg

---
+++ Team3DAC/ProductDac.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using Team3VO;
+
+
+namespace Team3DAC
+{
+    public class ProductDac : ConnectionAccess
+    {
+        public List<ProductVO> GetProductsAll()
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                string sql = "GetProductsAll";
+
+                cmd.Connection = new SqlConnection(this.ConnectionString);
+                cmd.CommandText = sql;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<ProductVO> list = Helper.DataReaderMapToList<ProductVO>(reader);
+                cmd.Connection.Close();
+                return list;
+            }
+        }
+    }
+}
