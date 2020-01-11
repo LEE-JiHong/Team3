81fdc6659199a84a4f3673fa0ea95878d1fdeda0
821051984868 <dnrmsaos@gmail.com>
2020-01-11 19:46:07 +0900

    kwg

---
+++ Team3VO/ProductPriceVO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team3VO
+{
+    class ProductPriceVO
+    {
+        private int price_id { get; set; }
+        private int product_id { get; set; }
+        private int company_id { get; set; }
+        private int price_present { get; set; }
+        private int price_past { get; set; }
+        private string price_sdate { get; set; }
+        private string price_edate { get; set; }
+        private string price_yn { get; set; }
+        private string price_uadmin { get; set; }
+        private string price_udate { get; set; }
+        private string price_comment { get; set; }
+        public ProductPriceVO()
+        {
+
+        }
+
+    }
+}
