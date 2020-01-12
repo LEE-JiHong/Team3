2617ad40f2654235125bfa63f7fac9c70600b26c
leejihong <alsch65@office.uc.ac.kr>
2020-01-13 01:17:34 +0900

    메인화면변경

--- Team3/DevForm/OrderMgt/DemandPlan.Designer.cs
+++ Team3/DevForm/OrderMgt/DemandPlan.Designer.cs
@@ -56,8 +56,9 @@
             // panel2
             // 
             this.panel2.Controls.Add(this.dataGridView1);
-            this.panel2.Location = new System.Drawing.Point(12, 153);
-            this.panel2.Size = new System.Drawing.Size(870, 475);
+            this.panel2.Location = new System.Drawing.Point(14, 191);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.panel2.Size = new System.Drawing.Size(1138, 595);
             // 
             // panel1
             // 
@@ -73,22 +74,28 @@
             this.panel1.Controls.Add(this.dateTimePicker1);
             this.panel1.Controls.Add(this.label12);
             this.panel1.ForeColor = System.Drawing.Color.Black;
-            this.panel1.Size = new System.Drawing.Size(870, 100);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.panel1.Size = new System.Drawing.Size(1138, 125);
             // 
             // label1
             // 
             this.label1.Image = global::Team3.Properties.Resources.list_menu;
             this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label1.Location = new System.Drawing.Point(15, 129);
-            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.Location = new System.Drawing.Point(17, 161);
+            this.label1.Size = new System.Drawing.Size(90, 19);
             this.label1.Text = "수요계획";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // panel3
             // 
             this.panel3.Controls.Add(this.flowLayoutPanel1);
-            this.panel3.Location = new System.Drawing.Point(651, 118);
-            this.panel3.Size = new System.Drawing.Size(232, 29);
+            this.panel3.Location = new System.Drawing.Point(887, 148);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.panel3.Size = new System.Drawing.Size(265, 36);
+            // 
+            // TopMenu
+            // 
+            this.TopMenu.Size = new System.Drawing.Size(1165, 75);
             // 
             // layoutButton
             // 
@@ -100,7 +107,8 @@
             // 
             // basepanel
             // 
-            this.basepanel.Size = new System.Drawing.Size(894, 642);
+            this.basepanel.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.basepanel.Size = new System.Drawing.Size(1165, 804);
             // 
             // imageList1
             // 
@@ -116,9 +124,11 @@
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonShadow;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(870, 475);
+            this.dataGridView1.Size = new System.Drawing.Size(1138, 595);
             this.dataGridView1.TabIndex = 50;
             // 
             // flowLayoutPanel1
@@ -129,8 +139,9 @@
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
             this.flowLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(232, 29);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(265, 36);
             this.flowLayoutPanel1.TabIndex = 16;
             // 
             // button2
@@ -141,9 +152,10 @@
             this.button2.ForeColor = System.Drawing.Color.Black;
             this.button2.Image = global::Team3.Properties.Resources.Editor_Edit;
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(132, 3);
+            this.button2.Location = new System.Drawing.Point(151, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(97, 23);
+            this.button2.Size = new System.Drawing.Size(111, 29);
             this.button2.TabIndex = 53;
             this.button2.Text = "생산계획생성";
             this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -157,9 +169,10 @@
             this.button3.ForeColor = System.Drawing.Color.Black;
             this.button3.Image = global::Team3.Properties.Resources.ModelEditor_GenerateContent;
             this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button3.Location = new System.Drawing.Point(65, 3);
+            this.button3.Location = new System.Drawing.Point(75, 4);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(61, 23);
+            this.button3.Size = new System.Drawing.Size(70, 29);
             this.button3.TabIndex = 54;
             this.button3.Text = "엑셀";
             this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -168,18 +181,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 23);
+            this.label2.Location = new System.Drawing.Point(26, 29);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.Size = new System.Drawing.Size(59, 20);
             this.label2.TabIndex = 57;
             this.label2.Text = "PlanID";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 61);
+            this.label3.Location = new System.Drawing.Point(26, 76);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(30, 16);
+            this.label3.Size = new System.Drawing.Size(33, 20);
             this.label3.TabIndex = 56;
             this.label3.Text = "업체";
             // 
@@ -187,9 +200,9 @@
             // 
             this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(459, 23);
+            this.label4.Location = new System.Drawing.Point(597, 29);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(17, 16);
+            this.label4.Size = new System.Drawing.Size(16, 20);
             this.label4.TabIndex = 62;
             this.label4.Text = "~";
             // 
@@ -197,27 +210,29 @@
             // 
             this.dateTimePicker2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker2.Location = new System.Drawing.Point(480, 19);
+            this.dateTimePicker2.Location = new System.Drawing.Point(621, 24);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(89, 22);
+            this.dateTimePicker2.Size = new System.Drawing.Size(101, 26);
             this.dateTimePicker2.TabIndex = 59;
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(368, 19);
+            this.dateTimePicker1.Location = new System.Drawing.Point(493, 24);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(86, 22);
+            this.dateTimePicker1.Size = new System.Drawing.Size(98, 26);
             this.dateTimePicker1.TabIndex = 58;
             // 
             // label12
             // 
             this.label12.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(606, 22);
+            this.label12.Location = new System.Drawing.Point(836, 28);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(30, 16);
+            this.label12.Size = new System.Drawing.Size(33, 20);
             this.label12.TabIndex = 57;
             this.label12.Text = "품목";
             // 
@@ -225,27 +240,28 @@
             // 
             this.label8.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(324, 22);
+            this.label8.Location = new System.Drawing.Point(442, 28);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(37, 16);
+            this.label8.Size = new System.Drawing.Size(45, 20);
             this.label8.TabIndex = 56;
             this.label8.Text = "Date";
             // 
             // comboBox1
             // 
-            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(77, 57);
+            this.comboBox1.Location = new System.Drawing.Point(94, 73);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(218, 24);
+            this.comboBox1.Size = new System.Drawing.Size(249, 28);
             this.comboBox1.TabIndex = 66;
             // 
             // textBox3
             // 
             this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox3.Location = new System.Drawing.Point(644, 19);
+            this.textBox3.Location = new System.Drawing.Point(879, 24);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(201, 22);
+            this.textBox3.Size = new System.Drawing.Size(229, 26);
             this.textBox3.TabIndex = 77;
             // 
             // button1
@@ -258,9 +274,10 @@
             this.button1.ForeColor = System.Drawing.Color.Black;
             this.button1.Image = global::Team3.Properties.Resources.Zoom_16x16;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(780, 57);
+            this.button1.Location = new System.Drawing.Point(1035, 71);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(65, 30);
+            this.button1.Size = new System.Drawing.Size(74, 38);
             this.button1.TabIndex = 78;
             this.button1.Text = "조회";
             this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -268,17 +285,19 @@
             // 
             // comboBox3
             // 
-            this.comboBox3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(77, 18);
+            this.comboBox3.Location = new System.Drawing.Point(94, 24);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(218, 24);
+            this.comboBox3.Size = new System.Drawing.Size(249, 28);
             this.comboBox3.TabIndex = 79;
             // 
             // DemandPlan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
-            this.ClientSize = new System.Drawing.Size(894, 724);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.ClientSize = new System.Drawing.Size(1165, 905);
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.MinimumSize = new System.Drawing.Size(1183, 872);
             this.Name = "DemandPlan";
             this.Tag = "수요계획";
             this.Text = "수요계획";
