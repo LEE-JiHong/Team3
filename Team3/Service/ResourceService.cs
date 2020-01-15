a8e285ea7e0b8ebe1bef18fa56bdc91dfa64eb3e
LEE-JiHong <leewlghd2@naver.com>
2020-01-15 19:32:21 +0900

    서비스 추가

---
+++ Team3/Service/ResourceService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Team3DAC;
+using Team3VO;
+ 
+
+namespace Team3
+{
+    public class ResourceService
+    {
+        /// <summary>
+        /// Company 모든컬럼 select
+        /// </summary>
+        public List<CompanyVO> GetCompanyAll()
+        {
+            ResourceDac dac = new ResourceDac();
+            return dac.GetCompanyAll();
+        }
+        /// <summary>
+        ///  Machine 설비 모든컬럼 select
+        /// </summary>
+        public List<MachineVO> GetMachineAll()
+        {
+            ResourceDac dac = new ResourceDac();
+            return dac.GetMachineAll();
+        }
+        /// <summary>
+        /// MachineGreade 설비군 모든컬럼 select
+        /// </summary>
+        public List<MachineGradeVO> GetMachineGrpAll()
+        {
+            ResourceDac dac = new ResourceDac();
+            return dac.GetMachineGrpAll();
+        }
+        public List<BORDB_VO> GetBORAll()
+        {
+            ResourceDac dac = new ResourceDac();
+            return dac.GetBORAll();
+        }
+        public List<FactoryDB_VO> GetFactoryAll()
+        {
+            ResourceDac dac = new ResourceDac();
+            return dac.GetFactoryAll();
+        }
+    }
+}
