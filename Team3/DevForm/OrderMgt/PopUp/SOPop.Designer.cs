84c9ad6b2c4d626640503e626be41556594e00e7
LEE-JiHong <leewlghd2@naver.com>
2020-01-20 16:11:48 +0900

    dialog base form border style 추가

--- Team3/DevForm/OrderMgt/PopUp/SOPop.Designer.cs
+++ Team3/DevForm/OrderMgt/PopUp/SOPop.Designer.cs
@@ -32,22 +32,18 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
             this.label16 = new System.Windows.Forms.Label();
-            this.label17 = new System.Windows.Forms.Label();
             this.txtSOWO = new System.Windows.Forms.TextBox();
-            this.txtODID = new System.Windows.Forms.TextBox();
             this.cbCompany = new System.Windows.Forms.ComboBox();
             this.cbDestination = new System.Windows.Forms.ComboBox();
             this.cbProduct = new System.Windows.Forms.ComboBox();
             this.txtOutput = new System.Windows.Forms.TextBox();
             this.txtCancel = new System.Windows.Forms.TextBox();
             this.txtOrder = new System.Windows.Forms.TextBox();
-            this.cbOrderGubun = new System.Windows.Forms.ComboBox();
             this.txtComment = new System.Windows.Forms.TextBox();
             this.dtpsDate = new System.Windows.Forms.DateTimePicker();
             this.panel3.SuspendLayout();
@@ -57,7 +53,7 @@
             // 
             // panel3
             // 
-            this.panel3.Location = new System.Drawing.Point(212, 436);
+            this.panel3.Location = new System.Drawing.Point(223, 402);
             // 
             // btnCancel
             // 
@@ -76,28 +72,24 @@
             // 
             this.panel2.Controls.Add(this.dtpsDate);
             this.panel2.Controls.Add(this.txtComment);
-            this.panel2.Controls.Add(this.cbOrderGubun);
             this.panel2.Controls.Add(this.txtOrder);
             this.panel2.Controls.Add(this.txtCancel);
             this.panel2.Controls.Add(this.txtOutput);
             this.panel2.Controls.Add(this.cbProduct);
             this.panel2.Controls.Add(this.cbDestination);
             this.panel2.Controls.Add(this.cbCompany);
-            this.panel2.Controls.Add(this.txtODID);
             this.panel2.Controls.Add(this.txtSOWO);
             this.panel2.Controls.Add(this.label13);
             this.panel2.Controls.Add(this.label14);
             this.panel2.Controls.Add(this.label15);
             this.panel2.Controls.Add(this.label16);
-            this.panel2.Controls.Add(this.label17);
             this.panel2.Controls.Add(this.label9);
-            this.panel2.Controls.Add(this.label8);
             this.panel2.Controls.Add(this.label6);
             this.panel2.Controls.Add(this.label5);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.label2);
             this.panel2.Location = new System.Drawing.Point(23, 84);
-            this.panel2.Size = new System.Drawing.Size(581, 346);
+            this.panel2.Size = new System.Drawing.Size(581, 307);
             // 
             // button1
             // 
@@ -145,21 +137,11 @@
             this.label6.TabIndex = 9;
             this.label6.Text = "출고수량";
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(14, 170);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(55, 15);
-            this.label8.TabIndex = 11;
-            this.label8.Text = "발주구분";
-            // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(14, 208);
+            this.label9.Location = new System.Drawing.Point(14, 169);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(31, 15);
             this.label9.TabIndex = 12;
@@ -169,7 +151,7 @@
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(294, 169);
+            this.label13.Location = new System.Drawing.Point(294, 133);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(55, 15);
             this.label13.TabIndex = 17;
@@ -179,7 +161,7 @@
             // 
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label14.Location = new System.Drawing.Point(294, 132);
+            this.label14.Location = new System.Drawing.Point(294, 96);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(55, 15);
             this.label14.TabIndex = 16;
@@ -190,7 +172,7 @@
             this.label15.AutoSize = true;
             this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label15.ForeColor = System.Drawing.Color.Orange;
-            this.label15.Location = new System.Drawing.Point(294, 96);
+            this.label15.Location = new System.Drawing.Point(294, 27);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(31, 15);
             this.label15.TabIndex = 15;
@@ -206,29 +188,12 @@
             this.label16.TabIndex = 14;
             this.label16.Text = "도착지";
             // 
-            // label17
-            // 
-            this.label17.AutoSize = true;
-            this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label17.Location = new System.Drawing.Point(294, 25);
-            this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(79, 15);
-            this.label17.TabIndex = 13;
-            this.label17.Text = "고객주문번호";
-            // 
             // txtSOWO
             // 
             this.txtSOWO.Location = new System.Drawing.Point(82, 23);
             this.txtSOWO.Name = "txtSOWO";
             this.txtSOWO.Size = new System.Drawing.Size(196, 21);
             this.txtSOWO.TabIndex = 19;
-            // 
-            // txtODID
-            // 
-            this.txtODID.Location = new System.Drawing.Point(374, 22);
-            this.txtODID.Name = "txtODID";
-            this.txtODID.Size = new System.Drawing.Size(196, 21);
-            this.txtODID.TabIndex = 20;
             // 
             // cbCompany
             // 
@@ -249,7 +214,7 @@
             // cbProduct
             // 
             this.cbProduct.FormattingEnabled = true;
-            this.cbProduct.Location = new System.Drawing.Point(374, 92);
+            this.cbProduct.Location = new System.Drawing.Point(374, 23);
             this.cbProduct.Name = "cbProduct";
             this.cbProduct.Size = new System.Drawing.Size(196, 23);
             this.cbProduct.TabIndex = 24;
@@ -264,29 +229,21 @@
             // 
             // txtCancel
             // 
-            this.txtCancel.Location = new System.Drawing.Point(374, 166);
+            this.txtCancel.Location = new System.Drawing.Point(374, 130);
             this.txtCancel.Name = "txtCancel";
             this.txtCancel.Size = new System.Drawing.Size(196, 21);
             this.txtCancel.TabIndex = 27;
             // 
             // txtOrder
             // 
-            this.txtOrder.Location = new System.Drawing.Point(374, 129);
+            this.txtOrder.Location = new System.Drawing.Point(374, 93);
             this.txtOrder.Name = "txtOrder";
             this.txtOrder.Size = new System.Drawing.Size(196, 21);
             this.txtOrder.TabIndex = 28;
             // 
-            // cbOrderGubun
-            // 
-            this.cbOrderGubun.FormattingEnabled = true;
-            this.cbOrderGubun.Location = new System.Drawing.Point(82, 167);
-            this.cbOrderGubun.Name = "cbOrderGubun";
-            this.cbOrderGubun.Size = new System.Drawing.Size(196, 23);
-            this.cbOrderGubun.TabIndex = 29;
-            // 
             // txtComment
             // 
-            this.txtComment.Location = new System.Drawing.Point(82, 206);
+            this.txtComment.Location = new System.Drawing.Point(82, 167);
             this.txtComment.Multiline = true;
             this.txtComment.Name = "txtComment";
             this.txtComment.Size = new System.Drawing.Size(488, 125);
@@ -303,7 +260,7 @@
             // SODialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
-            this.ClientSize = new System.Drawing.Size(628, 488);
+            this.ClientSize = new System.Drawing.Size(628, 466);
             this.Name = "SODialog";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.SODialog_Load);
@@ -318,7 +275,6 @@
         #endregion
 
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label3;
@@ -327,16 +283,13 @@
         private System.Windows.Forms.Label label14;
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.Label label16;
-        private System.Windows.Forms.Label label17;
         private System.Windows.Forms.TextBox txtComment;
-        private System.Windows.Forms.ComboBox cbOrderGubun;
         private System.Windows.Forms.TextBox txtOrder;
         private System.Windows.Forms.TextBox txtCancel;
         private System.Windows.Forms.TextBox txtOutput;
         private System.Windows.Forms.ComboBox cbProduct;
         private System.Windows.Forms.ComboBox cbDestination;
         private System.Windows.Forms.ComboBox cbCompany;
-        private System.Windows.Forms.TextBox txtODID;
         private System.Windows.Forms.TextBox txtSOWO;
         private System.Windows.Forms.DateTimePicker dtpsDate;
     }
