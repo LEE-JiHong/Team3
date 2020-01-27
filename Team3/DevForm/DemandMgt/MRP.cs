530d0905301e2f7193b607a4e2e1293309e1433f
leejihong <alsch65@office.uc.ac.kr>
2020-01-28 01:24:03 +0900

    mrp 조회 변경

--- Team3/DevForm/DemandMgt/MRP.cs
+++ Team3/DevForm/DemandMgt/MRP.cs
@@ -10,6 +10,8 @@ namespace Team3
 {
     public partial class MRP : Team3.VerticalGridBaseForm
     {
+        string planID = "20200121_P";
+
         public MRP()
         {
             InitializeComponent();
@@ -17,8 +19,20 @@ namespace Team3
 
         private void MRP_Load(object sender, EventArgs e)
         {
+            
             dtpStartDate.Value = DateTime.Now;
             dtpEndDate.Value = DateTime.Now.AddMonths(+1).AddDays(-1);
+
+            OrderService service = new OrderService();
+            dataGridView1.DataSource = service.GetMRP(planID, dtpStartDate.Value.ToShortDateString(), dtpEndDate.Value.ToShortDateString());
+        }
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+
+            OrderService service = new OrderService();
+            dataGridView1.DataSource = service.GetMRP(planID, dtpStartDate.Value.ToShortDateString(), dtpEndDate.Value.ToShortDateString());
         }
     }
 }
