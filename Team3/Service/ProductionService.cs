9f2387217de2bb75b6f7ebaa99813551844e55fb
kimjoonyoung88 <junchik3@gmail.com>
2020-01-29 18:00:41 +0900

    김준영 커밋

---
+++ Team3/Service/ProductionService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Team3DAC;
+using Team3VO;
+
+namespace Team3
+{
+    public class ProductionService
+    {
+        public List<BomVO> GetBomAll()
+        {
+            BomDac dac = new BomDac();
+            return dac.GetBomAll();
+        }
+
+    }
+}
