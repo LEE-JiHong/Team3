5dd59cb8744b7f14876ff7b59bc803653ec974b7
KimWooGeun <dnrmsaos@gmail.com>
2020-02-03 18:23:38 +0900

    kwg  마감관리 폼 추가 및 메인폼 메뉴와 연동

---
+++ Team3/DevForm/ShipmentMgt/ShipmentClosingMonthMgt.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Team3.DevForm.ShipmentMgt
+{
+    partial class ShipmentClosingMonthMgt
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShipmentClosingMonthMgt));
+            this.TopMenu.SuspendLayout();
+            this.basepanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Size = new System.Drawing.Size(1134, 388);
+            // 
+            // panel1
+            // 
+            this.panel1.Size = new System.Drawing.Size(1134, 116);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(561, 138);
+            // 
+            // TopMenu
+            // 
+            this.TopMenu.Size = new System.Drawing.Size(1026, 60);
+            // 
+            // layoutButton
+            // 
+            this.layoutButton.FlatAppearance.BorderSize = 0;
+            // 
+            // 닫기
+            // 
+            this.닫기.FlatAppearance.BorderSize = 0;
+            // 
+            // basepanel
+            // 
+            this.basepanel.Size = new System.Drawing.Size(1026, 512);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.Images.SetKeyName(0, "close.png");
+            this.imageList1.Images.SetKeyName(1, "layout.png");
+            this.imageList1.Images.SetKeyName(2, "menulist1.png");
+            // 
+            // ShipmentClosingMonthMgt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.ClientSize = new System.Drawing.Size(1026, 594);
+            this.Name = "ShipmentClosingMonthMgt";
+            this.Tag = "거래처별월마감";
+            this.TopMenu.ResumeLayout(false);
+            this.basepanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ Team3/DevForm/ShipmentMgt/ShipmentClosingMonthMgt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Team3.DevForm.ShipmentMgt
+{
+    public partial class ShipmentClosingMonthMgt : Team3.VerticalGridBaseForm
+    {
+        public ShipmentClosingMonthMgt()
+        {
+            InitializeComponent();
+        }
+    }
+}
