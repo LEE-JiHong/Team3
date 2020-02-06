ca7aba5e519f10d9a745a31adea54a16ac0c962a
LEE-JiHong <leewlghd2@naver.com>
2020-02-06 17:49:14 +0900

    자재입고 vo, service 생성

---
+++ Team3/Service/MaterialLedgerService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Team3VO;
+using Team3DAC;
+using System.Data;
+
+namespace Team3
+{
+    class MaterialLedgerService
+    {
+        public DataSet GetOrderList(string planID)
+        {
+            PurchasingDAC dac = new PurchasingDAC();
+            return dac.GetOrderList(planID);
+        }
+
+        public bool InsertOrder(List<OrderVO> list)
+        {
+            PurchasingDAC dac = new PurchasingDAC();
+            return dac.InsertOrder(list);
+        }
+
+        public DataTable GetOrderList()
+        {
+            PurchasingDAC dac = new PurchasingDAC();
+            return dac.GetOrderList();
+        }
+
+        public bool UpdateOrder(List<OrderVO> list)
+        {
+            PurchasingDAC dac = new PurchasingDAC();
+            return dac.UpdateOrder(list);
+        }
+
+        public bool UpdateOrderDate(OrderVO vo)
+        {
+            PurchasingDAC dac = new PurchasingDAC();
+            return dac.UpdateOrderDate(vo);
+        }
+    }
+}
