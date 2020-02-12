a706891652a7915419e0ec0f419ac9a6355e2be1
swg8491 <tladnrbs@naver.com>
2020-02-12 14:51:38 +0900

    swg

--- Team3VO/DMRVO.cs
+++ Team3VO/DMRVO.cs
@@ -8,7 +8,7 @@ namespace Team3VO
 {
     public class DMRVO
     {
-
+        public int pro_id { get; set; }
         public string plan_id { get; set; }//플랜ID
         public string product_id { get; set; }//상품ID
         public string product_codename { get; set; } //상품코드명
@@ -17,11 +17,26 @@ namespace Team3VO
         public string factory_name { get; set; }//창고 이름
         public string pro_count { get; set; }// pro_count*bom_use_count= 계획수량
         public string bom_use_count { get; set; }
-        public string w_count_present { get; set; }//현재고
-        public string w_count_past { get; set; }//이전재고
-        public string req_count { get; set; } //요청수량
+        public int w_count_present { get; set; }//현재고
+        public int w_count_past { get; set; }//이전재고
+        public string req_factory { get; set; }
+        public int req_count { get; set; } //요청수량
         public string req_date { get; set; }// 요청일
         public string reason { get; set; }//사유
+        public int nam { get; set; }
 
     }
+    public class DMRDBVO
+    {
+        public string product_id { get; set; }
+        public string product_codename { get; set; } //상품코드명
+        public string product_name { get; set; }//상품명
+        public string factory_name { get; set; }//창고 이름
+        public string req_factory { get; set; }
+        public int req_count { get; set; } //요청수량
+        public int w_count_present { get; set; }//현재고
+        public int w_count_past { get; set; }//이전재고
+        public int nam { get; set; }
+    }
+    
 }
