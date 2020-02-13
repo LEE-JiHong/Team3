9c1bc51de5ad1e2401c97c2886356cdad9443430
LEE-JiHong <leewlghd2@naver.com>
2020-02-13 14:53:35 +0900

    이지홍 자재재고현황

---
+++ Team3/Service/StockService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Team3VO;
+using Team3DAC;
+using System.Data;
+
+namespace Team3
+{
+    public class StockService
+    {
+
+        public List<CommonVO> GetProductType(string product_type)
+        {
+            StockDAC dac = new StockDAC();
+            return dac.GetProductType(product_type);
+        }
+
+        public DataTable GetMaterialStockList()
+        {
+            StockDAC dac = new StockDAC();
+            return dac.GetMaterialStockList();
+        }
+    }
+}
