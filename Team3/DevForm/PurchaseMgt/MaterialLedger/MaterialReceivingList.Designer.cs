031a2d1fb001352ad00d8f9eab56a68ac02e712b
leejihong <alsch65@office.uc.ac.kr>
2020-02-17 03:02:30 +0900

    이지홍 자재입고현황

--- Team3/DevForm/PurchaseMgt/MaterialLedger/MaterialReceivingList.Designer.cs
+++ Team3/DevForm/PurchaseMgt/MaterialLedger/MaterialReceivingList.Designer.cs
@@ -35,7 +35,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label10 = new System.Windows.Forms.Label();
             this.dtpEndDate = new System.Windows.Forms.DateTimePicker();
@@ -58,9 +58,9 @@
             // panel2
             // 
             this.panel2.Controls.Add(this.dataGridView1);
-            this.panel2.Location = new System.Drawing.Point(14, 185);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.panel2.Size = new System.Drawing.Size(1138, 522);
+            this.panel2.Location = new System.Drawing.Point(12, 148);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel2.Size = new System.Drawing.Size(996, 405);
             // 
             // panel1
             // 
@@ -77,29 +77,29 @@
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label11);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.panel1.Size = new System.Drawing.Size(1138, 118);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Size = new System.Drawing.Size(996, 94);
             // 
             // label1
             // 
             this.label1.Image = global::Team3.Properties.Resources.list_menu;
             this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label1.Location = new System.Drawing.Point(17, 157);
-            this.label1.Size = new System.Drawing.Size(122, 24);
+            this.label1.Location = new System.Drawing.Point(15, 126);
+            this.label1.Size = new System.Drawing.Size(107, 19);
             this.label1.Text = "자재입고현황";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // panel3
             // 
             this.panel3.Controls.Add(this.flowLayoutPanel1);
-            this.panel3.Location = new System.Drawing.Point(891, 143);
-            this.panel3.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.panel3.Size = new System.Drawing.Size(262, 36);
+            this.panel3.Location = new System.Drawing.Point(779, 114);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel3.Size = new System.Drawing.Size(229, 29);
             // 
             // TopMenu
             // 
-            this.TopMenu.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.TopMenu.Size = new System.Drawing.Size(1165, 75);
+            this.TopMenu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TopMenu.Size = new System.Drawing.Size(1019, 60);
             // 
             // layoutButton
             // 
@@ -111,8 +111,8 @@
             // 
             // basepanel
             // 
-            this.basepanel.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.basepanel.Size = new System.Drawing.Size(1165, 724);
+            this.basepanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.basepanel.Size = new System.Drawing.Size(1019, 578);
             // 
             // imageList1
             // 
@@ -124,9 +124,9 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 69);
+            this.label3.Location = new System.Drawing.Point(14, 55);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 20);
+            this.label3.Size = new System.Drawing.Size(30, 16);
             this.label3.TabIndex = 29;
             this.label3.Text = "품목";
             // 
@@ -134,9 +134,9 @@
             // 
             this.label11.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(437, 68);
+            this.label11.Location = new System.Drawing.Point(382, 54);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(69, 20);
+            this.label11.Size = new System.Drawing.Size(63, 16);
             this.label11.TabIndex = 39;
             this.label11.Text = "발주시리얼";
             // 
@@ -144,9 +144,9 @@
             // 
             this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(450, 22);
+            this.label4.Location = new System.Drawing.Point(394, 18);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(57, 20);
+            this.label4.Size = new System.Drawing.Size(52, 16);
             this.label4.TabIndex = 44;
             this.label4.Text = "납품업체";
             // 
@@ -154,23 +154,22 @@
             // 
             this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(818, 22);
+            this.label5.Location = new System.Drawing.Point(716, 18);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(57, 20);
+            this.label5.Size = new System.Drawing.Size(52, 16);
             this.label5.TabIndex = 43;
             this.label5.Text = "입고창고";
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Controls.Add(this.button2);
-            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.btnCancel);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
             this.flowLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(262, 36);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(229, 29);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // button2
@@ -180,30 +179,29 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Image = global::Team3.Properties.Resources.ExportToXLSX_16x16;
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(189, 4);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Location = new System.Drawing.Point(165, 3);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(70, 29);
+            this.button2.Size = new System.Drawing.Size(61, 23);
             this.button2.TabIndex = 49;
             this.button2.Text = "엑셀";
             this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button2.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // btnCancel
             // 
-            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
-            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Image = global::Team3.Properties.Resources.Recurrence_16x16;
-            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button3.Location = new System.Drawing.Point(88, 4);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(95, 29);
-            this.button3.TabIndex = 50;
-            this.button3.Text = "입고취소";
-            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button3.UseVisualStyleBackColor = false;
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.btnCancel.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Image = global::Team3.Properties.Resources.Recurrence_16x16;
+            this.btnCancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancel.Location = new System.Drawing.Point(76, 3);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(83, 23);
+            this.btnCancel.TabIndex = 50;
+            this.btnCancel.Text = "입고취소";
+            this.btnCancel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
             // 
             // dataGridView1
             // 
@@ -211,75 +209,68 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(1138, 522);
+            this.dataGridView1.Size = new System.Drawing.Size(996, 405);
             this.dataGridView1.TabIndex = 0;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(229, 24);
+            this.label10.Location = new System.Drawing.Point(200, 19);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(16, 20);
+            this.label10.Size = new System.Drawing.Size(17, 16);
             this.label10.TabIndex = 76;
             this.label10.Text = "~";
             // 
             // dtpEndDate
             // 
             this.dtpEndDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpEndDate.Location = new System.Drawing.Point(253, 19);
-            this.dtpEndDate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dtpEndDate.Location = new System.Drawing.Point(221, 15);
             this.dtpEndDate.Name = "dtpEndDate";
-            this.dtpEndDate.Size = new System.Drawing.Size(101, 26);
+            this.dtpEndDate.Size = new System.Drawing.Size(89, 22);
             this.dtpEndDate.TabIndex = 75;
             // 
             // dtpStartDate
             // 
             this.dtpStartDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpStartDate.Location = new System.Drawing.Point(120, 19);
-            this.dtpStartDate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dtpStartDate.Location = new System.Drawing.Point(105, 15);
             this.dtpStartDate.Name = "dtpStartDate";
-            this.dtpStartDate.Size = new System.Drawing.Size(101, 26);
+            this.dtpStartDate.Size = new System.Drawing.Size(89, 22);
             this.dtpStartDate.TabIndex = 74;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(120, 66);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Location = new System.Drawing.Point(105, 53);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(234, 26);
+            this.textBox1.Size = new System.Drawing.Size(205, 22);
             this.textBox1.TabIndex = 77;
             // 
             // textBox3
             // 
             this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.textBox3.Location = new System.Drawing.Point(516, 65);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox3.Location = new System.Drawing.Point(451, 52);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(234, 26);
+            this.textBox3.Size = new System.Drawing.Size(205, 22);
             this.textBox3.TabIndex = 81;
             // 
             // comboBox2
             // 
             this.comboBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(884, 17);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox2.Location = new System.Drawing.Point(773, 14);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(234, 28);
+            this.comboBox2.Size = new System.Drawing.Size(205, 24);
             this.comboBox2.TabIndex = 82;
             // 
             // comboBox3
             // 
             this.comboBox3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(516, 17);
-            this.comboBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox3.Location = new System.Drawing.Point(451, 14);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(234, 28);
+            this.comboBox3.Size = new System.Drawing.Size(205, 24);
             this.comboBox3.TabIndex = 83;
             // 
             // btnSearch
@@ -292,10 +283,9 @@
             this.btnSearch.ForeColor = System.Drawing.Color.Black;
             this.btnSearch.Image = global::Team3.Properties.Resources.Zoom_16x16;
             this.btnSearch.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSearch.Location = new System.Drawing.Point(1044, 62);
-            this.btnSearch.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSearch.Location = new System.Drawing.Point(913, 50);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(74, 38);
+            this.btnSearch.Size = new System.Drawing.Size(65, 30);
             this.btnSearch.TabIndex = 84;
             this.btnSearch.Text = "조회";
             this.btnSearch.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -305,18 +295,19 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 22);
+            this.label2.Location = new System.Drawing.Point(14, 18);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.Size = new System.Drawing.Size(41, 16);
             this.label2.TabIndex = 85;
             this.label2.Text = "입고일";
             // 
             // MaterialReceivingList
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
-            this.ClientSize = new System.Drawing.Size(1165, 825);
-            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.MinimumSize = new System.Drawing.Size(1147, 738);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.ClientSize = new System.Drawing.Size(1019, 660);
+            this.ImeMode = System.Windows.Forms.ImeMode.Hangul;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimumSize = new System.Drawing.Size(1006, 598);
             this.Name = "MaterialReceivingList";
             this.Tag = "자재입고현황";
             this.Text = "자재입고현황";
@@ -341,7 +332,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.DateTimePicker dtpEndDate;
--- Team3DAC/MaterialLedgerDAC.cs
+++ Team3DAC/MaterialLedgerDAC.cs
@@ -195,41 +195,11 @@ namespace Team3DAC
             {
                 StringBuilder sql = new StringBuilder();
 
-                sql.Append($"SELECT distinct w_id, w.plan_id, product_codename, product_name, factory_name, (w.w_count_present-w.w_count_past) as '입고량', order_serial, order_pdate FROM TBL_WAREHOUSE w inner join TBL_FACTORY f on w.factory_id = f.factory_id inner join TBL_ORDER o on o.plan_id = w.plan_id inner join TBL_PRODUCT p on p.product_id = w.product_id inner join TBL_COMPANY c on c.company_code = p.product_demand_com WHERE f.factory_type = 'FAC200'");
+                //sql.Append($"select wh_id, h.w_id, wh_udate, product_codename, product_name, factory_name, company_name, wh_product_count, order_id from TBL_WAREHOUSE_HIS h inner join TBL_PRODUCT p on h.product_id = p.product_id inner join TBL_WAREHOUSE w on h.w_id = w.w_id inner join TBL_FACTORY f on f.factory_id = w.factory_id inner join TBL_COMPANY c on c.company_code = p.product_demand_com where wh_category = 'P_ORDER_IN' order by wh_udate desc");
 
+                //sql.Append($"select order_pdate,product_codename, product_name,company_name,o.order_serial, (order_count-order_qcount) as order_qcount, order_count, factory_name from TBL_ORDER o inner join TBL_PRODUCT p on o.product_id = p.product_id  inner join TBL_COMPANY c on c.company_code = p.product_demand_com inner join TBL_FACTORY f on f.factory_code = p.product_in_sector where order_state = 'P_COMPLETE'");
 
-                //이지홍 
-//                @"select * 
-//from TBL_WAREHOUSE_HIS wh
-//--inner
-//join TBL_WAREHOUSE w on w.w_id = wh.wh_id
-//--inner
-//join TBL_FACTORY f on w.factory_id = f.factory_id
-//--inner
-//join TBL_PRODUCT p on p.product_id = w.product_id
-//--inner
-//join TBL_COMPANY c on c.company_code = p.product_demand_com
-//-- inner
-//join TBL_ORDER o on o.plan_id = w.plan_id
-//WHERE--f.factory_type = 'FAC200'
-//wh_category = 'P_ORDER_IN'
-//ORDER BY  wh_udate desc";
-
-                //if (vo.company_name != null)
-                //{
-                //    sql.Append(" and company_name = @company_name");
-                //    cmd.Parameters.AddWithValue("@company_name", vo.company_name);
-                //}
-
-                //if (vo.order_state != null)
-                //{
-                //    sql.Append($" and order_state = @order_state");
-                //    cmd.Parameters.AddWithValue("@order_state", vo.order_state);
-                //}
-                //else
-                //{
-                //    sql.Append($" and order_state != 'P_COMPLETE'");
-                //}
+                sql.Append($"select order_pdate,product_codename, product_name,company_name,o.order_serial, (order_count-order_qcount) as order_qcount, factory_name from TBL_ORDER o inner join TBL_PRODUCT p on o.product_id = p.product_id  inner join TBL_COMPANY c on c.company_code = p.product_demand_com inner join TBL_FACTORY f on f.factory_code = p.product_in_sector");
 
                 cmd.Connection = new SqlConnection(this.ConnectionString);
                 cmd.CommandText = sql.ToString();
@@ -250,5 +220,85 @@ namespace Team3DAC
                 return dataTable;
             }
         }
+
+        /// <summary>
+        /// 자재입고취소
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public bool CancelMaterial(List<MaterialReceivingVO> list)
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = new SqlConnection(this.ConnectionString);
+                cmd.Connection.Open();
+                SqlTransaction tran = cmd.Connection.BeginTransaction();
+
+                try
+                {
+                    cmd.Transaction = tran;
+                    cmd.CommandType = CommandType.Text;
+
+                    foreach (MaterialReceivingVO item in list)
+                    {
+                        cmd.Parameters.Clear();
+                        cmd.Parameters.AddWithValue("@order_serial", item.order_serial);
+
+                        cmd.CommandText = @"select plan_id from TBL_ORDER where order_serial = @order_serial";
+
+                        item.plan_id = cmd.ExecuteScalar().ToString();
+
+                        cmd.Parameters.AddWithValue("@plan_id", item.plan_id);
+                        cmd.Parameters.AddWithValue("@product_codename", item.product_codename);
+
+                        cmd.CommandText = @"select factory_id, product_id from TBL_PRODUCT p inner join TBL_FACTORY f on p.product_in_sector = f.factory_code where product_codename = @product_codename";
+
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        if (reader.Read())
+                        {
+                            item.factory_id = Convert.ToInt32(reader["factory_id"]);
+                            item.product_id = Convert.ToInt32(reader["product_id"]);
+                        }
+                        reader.Close();
+
+                        cmd.Parameters.AddWithValue("@factory_id", item.factory_id);
+                        cmd.Parameters.AddWithValue("@product_id", item.product_id);
+
+                        cmd.CommandText = @"select w_id from TBL_WAREHOUSE where plan_id = @plan_id and factory_id = @factory_id and product_id = @product_id";
+
+                        int result = Convert.ToInt32(cmd.ExecuteScalar());
+
+                        cmd.Parameters.AddWithValue("@w_id", result);
+                        cmd.Parameters.AddWithValue("@order_count", item.order_count);
+
+                        cmd.CommandText = "update TBL_ORDER set order_qcount = order_qcount + @order_count, order_state = 'P_WAIT' where order_serial = @order_serial";
+                        cmd.ExecuteNonQuery();
+
+                        cmd.CommandText = "update TBL_WAREHOUSE set w_count_present = w_count_present - @order_count, w_count_past = w_count_present where w_id = @w_id";
+                        cmd.ExecuteNonQuery();
+
+                        cmd.Parameters.AddWithValue("@wh_comment", item.product_name + " 입고취소");
+                        cmd.Parameters.AddWithValue("@wh_udate", DateTime.Now.ToShortDateString());
+
+                        cmd.CommandText = "insert into TBL_WAREHOUSE_HIS(w_id, product_id, order_id, wh_product_count, wh_udate, wh_comment, wh_category) values (@w_id, @product_id, @order_serial, @order_count, @wh_udate, @wh_comment, 'P_CANCEL_IN')";
+
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    tran.Commit();
+                    cmd.Connection.Close();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                    tran.Rollback();
+                    cmd.Connection.Close();
+                    return false;
+                }
+            }
+        }
+
     }
 }
