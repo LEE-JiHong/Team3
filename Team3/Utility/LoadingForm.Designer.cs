6c86ce1d1ae3ed6aa9c11ffa6e510aa93aa5bd95
leejihong <alsch65@office.uc.ac.kr>
2020-02-17 04:14:00 +0900

    이지홍 로딩폼 추가

---
+++ Team3/Utility/LoadingForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Team3
+{
+    partial class LoadingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // LoadingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(216, 204);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoadingForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LodingForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Team3/Utility/LoadingForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Team3
+{
+    public partial class LoadingForm : Form
+    {
+        public Action Function { get; set; }
+
+        private Image loadingImage;
+
+        public LoadingForm()
+
+        {
+
+            InitializeComponent();
+
+
+            this.loadingImage = Image.FromFile(@"C:\Users\alsch\source\repos\LEE-JiHong\Team3\Team3\Resources\loading.gif");
+
+            this.Shown += new EventHandler(Form_Loaded);
+
+
+        }
+
+        private void Form_Loaded(object sender, EventArgs e)
+
+        {
+
+            var thread = new Thread(
+
+                () =>
+
+                {
+
+                    Function?.Invoke();
+
+                    this.Invoke(
+
+                        (Action)(() =>
+
+                        {
+
+                            this.Close();
+
+                        }));
+
+                });
+
+            thread.Start();
+
+        }
+
+    }
+}
