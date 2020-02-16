7319e514041dd6d1ecccf03aca880b728b980edc
leejihong <alsch65@office.uc.ac.kr>
2020-02-17 08:12:09 +0900

    김우근 매출마감 쿼리

--- Team3DAC/ShipmentDac.cs
+++ Team3DAC/ShipmentDac.cs
@@ -84,6 +84,79 @@ namespace Team3DAC
                 }
             }
         }
+
+        public bool EndProcessing(List<ShipmentOutVO> list)
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = new SqlConnection(this.ConnectionString);
+                cmd.Connection.Open();
+                SqlTransaction tran = cmd.Connection.BeginTransaction();
+
+                try
+                {
+                    cmd.Transaction = tran;
+                    cmd.CommandType = CommandType.Text;
+
+                    //int num = 1;
+
+                    foreach (var item in list)
+                    {
+                        cmd.Parameters.Clear();
+
+                        cmd.Parameters.AddWithValue("@s_company", item.company_code);
+                        cmd.Parameters.AddWithValue("@so_id", item.so_id);
+                        cmd.Parameters.AddWithValue("@plan_id", item.plan_id);
+                        cmd.Parameters.AddWithValue("@s_date", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+
+                        cmd.CommandText = @"select company_id from TBL_COMPANY where company_code = @company_code";
+
+                        int company_id = Convert.ToInt32(cmd.ExecuteScalar());
+
+                        cmd.Parameters.AddWithValue("@product_id", item.product_id);
+                        cmd.Parameters.AddWithValue("@company_id", company_id); 
+
+                        cmd.CommandText = @"select price_present from TBL_P_PRICE where product_id = @product_id and company_id = @company_id and price_edate = '9999-12-31'";
+
+                        int price = Convert.ToInt32(cmd.ExecuteScalar());
+
+                        cmd.Parameters.AddWithValue("@s_count", item.incount);
+                        cmd.Parameters.AddWithValue("@s_TotalPrice", price * item.incount);
+
+                        cmd.CommandText = @"insert into TBL_SALES_COMPLETE(s_date, so_id, product_id, plan_id, s_count, s_TotalPrice, s_company) values (@s_date, @so_id, @product_id, @plan_id, @s_count, @s_TotalPrice, @s_company)";
+                        cmd.ExecuteNonQuery();
+
+                        cmd.CommandText = @"update TBL_SO_MASTER set so_ocount = so_ocount + @incount where so_id = @so_id";
+                        cmd.ExecuteNonQuery();
+                            
+                        cmd.CommandText = @"select w_id from TBL_WAREHOUSE w inner join TBL_FACTORY f on f.factory_id = w.factory_id where plan_id = @plan_id and product_id = @product_id and f.factory_type = 'FAC700'";
+
+                        int w_id = Convert.ToInt32(cmd.ExecuteScalar());
+
+                        cmd.Parameters.AddWithValue("@w_id", item.w_id);
+
+                        cmd.CommandText = @"update TBL_WAREHOUSE set w_count_present = w_count_present - @incount, w_count_past = w_count_present where w_id = @w_id";
+                        cmd.ExecuteNonQuery();
+
+                        cmd.Parameters.AddWithValue("@wh_udate", DateTime.Now.ToShortDateString());
+
+                        cmd.CommandText = "insert into TBL_WAREHOUSE_HIS(w_id, product_id,  wh_product_count, wh_udate, wh_comment, wh_category) values (@w_id, @product_id, @incount, @wh_udate, '매출마감', 'P_SALES_COMPLETE')";
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    tran.Commit();
+                    cmd.Connection.Close();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                    tran.Rollback();
+                    cmd.Connection.Close();
+                    return false;
+                }
+            }
+        }
     }
 }
 
--- Team3VO/ShipmentVO.cs
+++ Team3VO/ShipmentVO.cs
@@ -33,4 +33,35 @@ namespace Team3VO
         public int transfer_count { get; set; }
 
     }
+
+    public class ShipmentOutVO
+    {
+        public int so_id { get; set; }
+        public int so_ocount { get; set; }
+        public int so_ccount { get; set; }
+        public int so_pcount { get; set; }
+        public int w_count_present { get; set; }
+        public int product_id { get; set; }
+        public string so_edate { get; set; }
+        public string so_sdate { get; set; }
+        public string plan_id { get; set; }
+        public string company_code { get; set; }
+        public string company_type { get; set; }
+        public string product_name { get; set; }
+        public string product_codename { get; set; }
+        public string from_wh { get; set; }
+        public string from_wh_value { get; set; }
+        public string to_wh { get; set; }
+        public string to_wh_value { get; set; }
+        public int uadmin { get; set; }
+        public string wh_comment { get; set; }
+        public string factory_name { get; set; }
+        public string category { get; set; }
+        public string udate { get; set; }
+        public int transfer_count { get; set; }
+        public string company_name { get; set; }
+
+        public int incount { get; set; }
+        public int w_id { get; set; }
+    }
 }
